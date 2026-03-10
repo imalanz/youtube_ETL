3ebaef5e16136eb0d39153d38beab0f0ce2fd91d
imanol <arq.imalanz@gmail.com>
2026-03-10 11:37:34 +0100

    get_video and save to json

--- video_stats.py
+++ video_stats.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+import json 
 
 load_dotenv(dotenv_path="./.env")
 
@@ -52,7 +54,57 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e
 
+
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_ids_list, batch_size=50):
+        for video_id in range(0, len(video_ids_list), batch_size):
+            yield video_ids_list[video_id:video_id+batch_size]
+ 
+    try:
+        for batch in batch_list(video_ids, max_results):
+            video_ids_str = ",".join(batch)
+            
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics&id={video_ids_str}&key={API_KEY}"
+            
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                content_details = item.get("contentDetails", {})
+                statistics = item["statistics"]
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "published_date": snippet["publishedAt"],
+                    "duration": content_details.get("duration", None),
+                    "view_count": statistics.get("viewCount", None),
+                    "like_count": statistics.get("likeCount", None),
+                    "comment_count": statistics.get("commentCount", None),
+                }
+                extracted_data.append(video_data)
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def save_to_json(extracted_data):
+    file_path = f"./data/youtube_data_{datetime.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
-    get_video_ids(playlist_id)
+    video_ids = get_video_ids(playlist_id)
+    extracted_data = extract_video_data(video_ids)
+    save_to_json(extracted_data)
     
